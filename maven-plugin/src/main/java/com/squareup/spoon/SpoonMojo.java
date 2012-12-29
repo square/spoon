a5c57431ab2a12dc74012af7bcac7943f927f672
Jake Wharton <jw@squareup.com>
2012-12-28 22:04:33 -0800

    Proper classpath resolution inside the Maven plugin.

--- maven-plugin/src/main/java/com/squareup/spoon/SpoonMojo.java
+++ maven-plugin/src/main/java/com/squareup/spoon/SpoonMojo.java
@@ -2,13 +2,19 @@
 package com.squareup.spoon;
 
 import java.io.File;
+import java.util.Iterator;
+import java.util.Set;
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactResolutionRequest;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.repository.RepositorySystem;
 
 import static com.squareup.spoon.Main.DEFAULT_TITLE;
 import static com.squareup.spoon.Main.OUTPUT_DIRECTORY_NAME;
@@ -49,24 +55,18 @@ public class SpoonMojo extends AbstractMojo {
   @Parameter(property = "project", required = true, readonly = true)
   private MavenProject project;
 
+  @Parameter(property = "localRepository", readonly = true, required = true)
+  private ArtifactRepository local;
+
+  @Component
+  private RepositorySystem repositorySystem;
+
   public void execute() throws MojoExecutionException {
     Log log = getLog();
 
     if (mavenTestSkip || mavenSkipTests) {
       log.info("Skipping Spoon execution.");
       return;
-    }
-
-    Artifact self = null;
-    for (Artifact artifact : project.getPluginArtifacts()) {
-      if ("com.squareup.spoon".equals(artifact.getGroupId()) //
-          && "spoon-maven-plugin".equals(artifact.getArtifactId())) {
-        self = artifact;
-        break;
-      }
-    }
-    if (self == null) {
-      throw new MojoExecutionException("Could not find representation of this plugin in project.");
     }
 
     boolean hasError = false;
@@ -106,14 +106,54 @@ public class SpoonMojo extends AbstractMojo {
       throw new MojoExecutionException("Unable to invoke Spoon. See console for details.");
     }
 
-    String classpath = System.getProperty("java.class.path"); // TODO infer from artifact.
-    File output = new File(outputDirectory, OUTPUT_DIRECTORY_NAME);
-
-    log.debug("Output directory: " + output.getAbsolutePath());
-    log.debug("Spoon title: " + title);
-    log.debug("Debug: " + Boolean.toString(debug));
+    Artifact self = getArtifactForSelf();
+    Set<Artifact> selfWithDeps = getDependenciesForArtifact(self);
+    String classpath = getClasspath(selfWithDeps);
     log.debug("Classpath: " + classpath);
 
+    File output = new File(outputDirectory, OUTPUT_DIRECTORY_NAME);
+    log.debug("Output directory: " + output.getAbsolutePath());
+
+    log.debug("Spoon title: " + title);
+    log.debug("Debug: " + Boolean.toString(debug));
+
     new ExecutionSuite(title, androidSdk, app, instrumentation, output, debug, classpath).run();
+  }
+
+  private Artifact getArtifactForSelf() throws MojoExecutionException {
+    for (Artifact artifact : project.getPluginArtifacts()) {
+      if ("com.squareup.spoon".equals(artifact.getGroupId()) //
+          && "spoon-maven-plugin".equals(artifact.getArtifactId())) {
+        return artifact;
+      }
+    }
+    throw new MojoExecutionException("Could not find representation of this plugin in project.");
+  }
+
+  private Set<Artifact> getDependenciesForArtifact(Artifact artifact) {
+    ArtifactResolutionRequest arr = new ArtifactResolutionRequest()
+        .setArtifact(artifact)
+        .setResolveTransitively(true)
+        .setLocalRepository(local);
+    return repositorySystem.resolve(arr).getArtifacts();
+  }
+
+  private String getClasspath(Set<Artifact> artifacts) {
+    if (artifacts == null || artifacts.isEmpty()) {
+      return "";
+    }
+    StringBuilder builder = new StringBuilder();
+    Iterator<Artifact> i = artifacts.iterator();
+    if (i.hasNext()) {
+      builder.append(getLocalPathToArtifact(i.next()));
+      while (i.hasNext()) {
+        builder.append(File.pathSeparator).append(getLocalPathToArtifact(i.next()));
+      }
+    }
+    return builder.toString();
+  }
+
+  private String getLocalPathToArtifact(Artifact artifact) {
+    return new File(local.getBasedir(), local.pathOf(artifact)).getAbsolutePath();
   }
 }
