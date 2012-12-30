7e3191b48b456f68d049777a9c41b3152217a912
Jake Wharton <jw@squareup.com>
2012-12-30 01:58:08 -0800

    Remove test script. You can now test using Maven.

--- test-cli.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-java -jar spoon/target/spoon-*-with-dep*.jar \
-    --apk sample/app/target/spoon-sample-app-1.0.0-SNAPSHOT.apk \
-    --test-apk sample/tests/target/spoon-sample-tests-1.0.0-SNAPSHOT.apk \
-    --title "Spoon Sample" \
-    --debug
