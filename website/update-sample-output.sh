15511753857bbc39d428084baa976a7f304a0b94
Jake Wharton <jw@squareup.com>
2012-12-30 22:30:35 -0800

    Include sample on the website.

---
+++ website/update-sample-output.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo ""
+echo "Make sure test devices are plugged in and recognized by ADB."
+echo ""
+echo "Press [enter] when ready..."
+read
+
+set -ex
+
+git rm -rf sample
+cd ..
+mvn clean verify
+cp -R sample/tests/target/spoon-output website/sample
+cd -
+
+echo "Sample output updated. Make sure to commit the changes."
