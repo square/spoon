a44ac109dde95aa307e496fc3a200beb47d40bf8
Jake Wharton <jw@squareup.com>
2013-01-04 16:31:36 -0800

    Fix test-cli.

--- test-cli.sh
+++ test-cli.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-APK=`\ls sample/app/target/*.apk`
-TEST_APK=`\ls sample/tests/target/*.apk`
+APK=`\ls spoon-sample/app/target/*.apk`
+TEST_APK=`\ls spoon-sample/tests/target/*.apk`
 
 java -jar spoon/target/spoon-*-jar-with-dependencies.jar --apk "$APK" --test-apk "$TEST_APK" --output target
 
