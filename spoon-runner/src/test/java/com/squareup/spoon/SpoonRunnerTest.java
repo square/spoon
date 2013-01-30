026313a389d0390b2fc52697fba0f0fb7005a31e
Jake Wharton <jw@squareup.com>
2013-01-30 09:20:42 -0800

    Ensure we mark run as failed for top-level exceptions.

---
+++ spoon-runner/src/test/java/com/squareup/spoon/SpoonRunnerTest.java
@@ -0,0 +1,97 @@
+package com.squareup.spoon;
+
+import org.junit.Test;
+
+import static com.squareup.spoon.SpoonRunner.parseOverallSuccess;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class SpoonRunnerTest {
+  @Test public void parsingSuccess() {
+    SpoonSummary summary;
+    DeviceTest device = new DeviceTest("foo", "bar");
+
+    // PASS: No devices attached.
+    summary = new SpoonSummary.Builder() //
+        .setTitle("test") //
+        .build(); //
+    assertThat(parseOverallSuccess(summary)).isTrue();
+
+    // FAIL: Unable to install application.
+    summary = new SpoonSummary.Builder() //
+        .setTitle("test") //
+        .start() //
+        .addResult("123", new DeviceResult.Builder() //
+            .markInstallAsFailed("") //
+            .build()) //
+        .end() //
+        .build(); //
+    assertThat(parseOverallSuccess(summary)).isFalse();
+
+    // FAIL: Top-level exception, no tests run.
+    summary = new SpoonSummary.Builder() //
+        .setTitle("test") //
+        .start() //
+        .addResult("123", new DeviceResult.Builder() //
+            .addException(new RuntimeException()) //
+            .build()) //
+        .end() //
+        .build(); //
+    assertThat(parseOverallSuccess(summary)).isFalse();
+
+    // PASS: No tests run.
+    summary = new SpoonSummary.Builder() //
+        .setTitle("test") //
+        .start() //
+        .addResult("123", new DeviceResult.Builder() //
+            .startTests() //
+            .endTests() //
+            .build()) //
+        .end() //
+        .build(); //
+    assertThat(parseOverallSuccess(summary)).isTrue();
+
+    // FAIL: Test failure.
+    summary = new SpoonSummary.Builder() //
+        .setTitle("test") //
+        .start() //
+        .addResult("123", new DeviceResult.Builder() //
+            .startTests() //
+            .addTestResultBuilder(device, new DeviceTestResult.Builder() //
+                .startTest() //
+                .markTestAsFailed("Failed")
+                .endTest()) //
+            .build()) //
+        .end() //
+        .build(); //
+    assertThat(parseOverallSuccess(summary)).isFalse();
+
+    // FAIL: Test error.
+    summary = new SpoonSummary.Builder() //
+        .setTitle("test") //
+        .start() //
+        .addResult("123", new DeviceResult.Builder() //
+            .startTests() //
+            .addTestResultBuilder(device, new DeviceTestResult.Builder() //
+                .startTest() //
+                .markTestAsError("Failed")
+                .endTest()) //
+            .build()) //
+        .end() //
+        .build(); //
+    assertThat(parseOverallSuccess(summary)).isFalse();
+
+    // PASS: Test success.
+    summary = new SpoonSummary.Builder() //
+        .setTitle("test") //
+        .start() //
+        .addResult("123", new DeviceResult.Builder() //
+            .startTests() //
+            .addTestResultBuilder(device, new DeviceTestResult.Builder() //
+                .startTest() //
+                .endTest()) //
+            .build()) //
+        .end() //
+        .build(); //
+    assertThat(parseOverallSuccess(summary)).isTrue();
+  }
+}
