c919096d3ed4726df2f35a36c2288d22873cca4a
Jake Wharton <jw@squareup.com>
2013-02-13 10:07:22 -0800

    Website at Wed Feb 13 10:07:22 PST 2013

---
+++ update-sample-output.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo ""
+echo "Make sure test devices are plugged in and recognized by ADB."
+echo ""
+adb devices
+echo ""
+echo "Press [enter] when ready..."
+read
+
+set -ex
+
+rm -rf sample
+cd ..
+mvn clean install -e
+cp -R spoon-sample/tests/target/spoon-output website/sample
+cd -
+git add sample
+git add -u sample
+
+echo "Sample output updated. Make sure to commit the changes."
