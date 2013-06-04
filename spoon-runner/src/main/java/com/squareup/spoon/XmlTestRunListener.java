dc622fd4b5e5367f233fda87752571403c282b12
Jake Wharton <jw@squareup.com>
2013-06-04 13:08:50 -0700

    Add JUnit XML report output for each device.
    Closes #100.

---
+++ spoon-runner/src/main/java/com/squareup/spoon/XmlTestRunListener.java
@@ -0,0 +1,24 @@
+package com.squareup.spoon;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * An {@link com.android.ddmlib.testrunner.XmlTestRunListener XmlTestRunListener} that points
+ * directly to an output file.
+ */
+class XmlTestRunListener extends com.android.ddmlib.testrunner.XmlTestRunListener {
+  private final File file;
+
+  XmlTestRunListener(File file) {
+    if (file == null) {
+      throw new IllegalArgumentException("File may not be null.");
+    }
+    this.file = file;
+  }
+
+  @Override protected File getResultFile(File reportDir) throws IOException {
+    file.getParentFile().mkdirs();
+    return file;
+  }
+}
