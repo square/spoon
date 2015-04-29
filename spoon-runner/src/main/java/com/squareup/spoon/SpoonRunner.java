acbb6fa5bb68fc08fe3c772b0f8a54bf7852741b
Fyzxs <fyzxs@users.noreply.github.com>
2015-04-29 10:12:53 -0700

    Removing line break

--- spoon-runner/src/main/java/com/squareup/spoon/SpoonRunner.java
+++ spoon-runner/src/main/java/com/squareup/spoon/SpoonRunner.java
@@ -392,8 +392,8 @@ public final class SpoonRunner {
     public File testApk;
 
     @Parameter(names = { "--e" },
-        description = "-e Arguments to pass to the Instrumentation Runner. This can be used multiple times for multiple entries." +
-            "Example --e \"<NAME> <VALUE>\". Quotes are required." )
+        description = "Arguments to pass to the Instrumentation Runner. This can be used multiple times for multiple entries." +
+            " Usage: --e \"<NAME> <VALUE>\". Quotes are required." )
     public List<String> instrumentationArgs;
 
     @Parameter(names = { "--class-name" }, description = "Test class name to run (fully-qualified)")
