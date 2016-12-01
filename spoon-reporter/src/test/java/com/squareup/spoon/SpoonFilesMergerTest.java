3b3bcfdb72b18d90ab18904aa9ab560eb518638a
Theo van den Bogaart <todorus@todorus.com>
2016-12-01 14:41:52 +0100

    $402 files merger copies screenshots to new location

--- spoon-reporter/src/test/java/com/squareup/spoon/SpoonFilesMergerTest.java
+++ spoon-reporter/src/test/java/com/squareup/spoon/SpoonFilesMergerTest.java
@@ -1,16 +1,16 @@
 package com.squareup.spoon;
 
-import com.android.ddmlib.Log;
 import com.google.common.io.Files;
 import com.google.gson.Gson;
-import org.junit.Assert;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileReader;
-import java.util.Collection;
+import java.io.IOException;
 import java.util.List;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class SpoonFilesMergerTest {
@@ -35,13 +35,17 @@ public class SpoonFilesMergerTest {
     for(int i = 0; i < originalResults.length; i++){
       SpoonSummary original = gson.fromJson(new FileReader(originalResults[i]), SpoonSummary.class);
       SpoonSummary modified = gson.fromJson(new FileReader(modifiedResults[i]), SpoonSummary.class);
-      checkModifiedFiles(outputDir, original, modified);
+      checkModifiedPaths(outputDir, original, modified);
     }
     // And copy the files to these new paths
-    // TODO find out if this is testable with temporary files
+    for(int i = 0; i < originalResults.length; i++){
+      SpoonSummary original = gson.fromJson(new FileReader(originalResults[i]), SpoonSummary.class);
+      SpoonSummary modified = gson.fromJson(new FileReader(modifiedResults[i]), SpoonSummary.class);
+      checkCopies(original, modified);
+    }
   }
 
-  private void checkModifiedFiles(File outputDir, SpoonSummary original, SpoonSummary modified) {
+  private void checkModifiedPaths(File outputDir, SpoonSummary original, SpoonSummary modified) {
     DeviceResult[] originalDeviceResults = new DeviceResult[original.getResults().values().size()];
     original.getResults().values().toArray(originalDeviceResults);
     DeviceResult[] modifiedDeviceResults = new DeviceResult[modified.getResults().values().size()];
@@ -74,4 +78,38 @@ public class SpoonFilesMergerTest {
     }
   }
 
+  private void checkCopies(SpoonSummary original, SpoonSummary modified) throws IOException {
+    DeviceResult[] originalDeviceResults = new DeviceResult[original.getResults().values().size()];
+    original.getResults().values().toArray(originalDeviceResults);
+    DeviceResult[] modifiedDeviceResults = new DeviceResult[modified.getResults().values().size()];
+    modified.getResults().values().toArray(modifiedDeviceResults);
+
+    for(int i=0; i < originalDeviceResults.length; i++){
+      DeviceResult originalDeviceResult = originalDeviceResults[i];
+      DeviceResult modifiedDeviceResult = modifiedDeviceResults[i];
+
+      DeviceTestResult[] originalDeviceTestResults = new DeviceTestResult[originalDeviceResult.getTestResults().size()];
+      originalDeviceResult.getTestResults().values().toArray(originalDeviceTestResults);
+      DeviceTestResult[] modifiedDeviceTestResults = new DeviceTestResult[modifiedDeviceResult.getTestResults().size()];
+      modifiedDeviceResult.getTestResults().values().toArray(modifiedDeviceTestResults);
+
+      for(int j=0; j < originalDeviceTestResults.length; j++){
+        DeviceTestResult originalDeviceTestResult = originalDeviceTestResults[j];
+        DeviceTestResult modifiedDeviceTestResult = modifiedDeviceTestResults[j];
+
+        List<File> originalScreenshots = originalDeviceTestResult.getScreenshots();
+        List<File> modifiedScreenshots = modifiedDeviceTestResult.getScreenshots();
+
+        for(int k=0; k < originalScreenshots.size(); k++) {
+          File originalScreenshot = originalScreenshots.get(k);
+          File modifiedScreenshot = modifiedScreenshots.get(k);
+
+          byte[] originalData = FileUtils.readFileToByteArray(originalScreenshot);
+          byte[] modifiedData = FileUtils.readFileToByteArray(modifiedScreenshot);
+
+          assertArrayEquals(originalData, modifiedData);
+        }
+      }
+    }
+  }
 }
