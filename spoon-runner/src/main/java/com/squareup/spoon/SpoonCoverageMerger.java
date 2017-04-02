67fd7451acef5e727ebda2e1c77eeaf66a3648be
Jake Wharton <jw@squareup.com>
2017-04-01 23:57:47 -0400

    Add some Java 8-isms to coverage merger.

--- spoon-runner/src/main/java/com/squareup/spoon/SpoonCoverageMerger.java
+++ spoon-runner/src/main/java/com/squareup/spoon/SpoonCoverageMerger.java
@@ -1,17 +1,14 @@
 package com.squareup.spoon;
 
-import com.google.common.base.Function;
-import org.jacoco.core.tools.ExecFileLoader;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Set;
+import org.jacoco.core.tools.ExecFileLoader;
 
 import static com.google.common.collect.Collections2.transform;
 import static com.squareup.spoon.SpoonDeviceRunner.COVERAGE_DIR;
 import static com.squareup.spoon.SpoonDeviceRunner.COVERAGE_FILE;
-import static com.squareup.spoon.SpoonUtils.sanitizeSerial;
 
 final class SpoonCoverageMerger {
   private static final String MERGED_COVERAGE_FILE = "merged-coverage.ec";
@@ -22,21 +19,12 @@ final class SpoonCoverageMerger {
   }
 
   public void mergeCoverageFiles(Set<String> serials, File outputDirectory) throws IOException {
-    Collection<String> sanitizeSerials = transform(serials, toSanitizeSerials());
+    Collection<String> sanitizeSerials = transform(serials, SpoonUtils::sanitizeSerial);
     for (String serial : sanitizeSerials) {
       String coverageFilePath = COVERAGE_DIR + "/" + serial + "/" + COVERAGE_FILE;
       execFileLoader.load(new File(outputDirectory, coverageFilePath));
     }
     String mergedCoverageFile = COVERAGE_DIR + "/" + MERGED_COVERAGE_FILE;
     execFileLoader.save(new File(outputDirectory, mergedCoverageFile), false);
-  }
-
-  private Function<String, String> toSanitizeSerials() {
-    return new Function<String, String>() {
-      @Override
-      public String apply(String serials) {
-        return sanitizeSerial(serials);
-      }
-    };
   }
 }
