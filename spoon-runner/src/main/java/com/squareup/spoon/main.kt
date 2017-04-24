ffbfdf0818550b48f7e299bcff28cbf649bc9c8c
Jake Wharton <jw@squareup.com>
2017-04-24 19:03:37 -0400

    Update argparser to 2.0.
    This lets us do multi-value args, better nullability, and prettier output.

--- spoon-runner/src/main/java/com/squareup/spoon/main.kt
+++ spoon-runner/src/main/java/com/squareup/spoon/main.kt
@@ -2,34 +2,36 @@
 package com.squareup.spoon
 
 import com.xenomachina.argparser.ArgParser
-import com.xenomachina.argparser.runMain
+import com.xenomachina.argparser.mainBody
 
 fun main(vararg args: String) {
-  CliArgs(ArgParser(args)).runMain("spoon-runner") {
-    val builder = SpoonRunner.Builder()
-    builder.setApplicationApk(mainApk)
-    builder.setInstrumentationApk(testApk)
-    sdk?.let(builder::setAndroidSdk)
-    title?.let(builder::setTitle)
-    builder.setInstrumentationArgs(instrumentationArgs);
-    className?.let(builder::setClassName)
-    methodName?.let(builder::setMethodName)
-    size?.let(builder::setTestSize)
-    output?.let(builder::setOutputDirectory)
-    builder.setFailIfNoDeviceConnected(failIfNoDevices)
-    builder.setSequential(sequential)
-    initScript?.let(builder::setInitScript)
-    builder.setGrantAll(grantAll)
-    builder.setNoAnimations(disableGif)
-    adbTimeout?.let(builder::setAdbTimeout)
-    serials.forEach { builder.addDevice(it) }
-    skipSerials.forEach { builder.addDevice(it) }
-    builder.setShard(shard)
-    builder.setDebug(debug)
-    builder.setCodeCoverage(coverage)
+  mainBody("spoon-runner") {
+    CliArgs(ArgParser(args)).run {
+      val builder = SpoonRunner.Builder()
+      builder.setApplicationApk(mainApk)
+      builder.setInstrumentationApk(testApk)
+      sdk?.let(builder::setAndroidSdk)
+      title?.let(builder::setTitle)
+      builder.setInstrumentationArgs(instrumentationArgs);
+      className?.let(builder::setClassName)
+      methodName?.let(builder::setMethodName)
+      size?.let(builder::setTestSize)
+      output?.let(builder::setOutputDirectory)
+      builder.setFailIfNoDeviceConnected(failIfNoDevices)
+      builder.setSequential(sequential)
+      initScript?.let(builder::setInitScript)
+      builder.setGrantAll(grantAll)
+      builder.setNoAnimations(disableGif)
+      adbTimeout?.let(builder::setAdbTimeout)
+      serials.forEach { builder.addDevice(it) }
+      skipSerials.forEach { builder.addDevice(it) }
+      builder.setShard(shard)
+      builder.setDebug(debug)
+      builder.setCodeCoverage(coverage)
 
-    if (!builder.build().run() && failOnFailure) {
-      System.exit(1)
+      if (!builder.build().run() && failOnFailure) {
+        System.exit(1)
+      }
     }
   }
 }
