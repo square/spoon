bce3aeae4150dce07018e3bf05761205be211ab2
Monte <montecreasor@gmail.com>
2017-07-17 07:00:41 -0400

    Added code to handle parsing APK manifests that have string pools encoded with UTF-8. Added JUnit test to test UTF-8 and UTF-16 parsing with 2 accompanying APKs (one is UTF-8 and the other is UTF-16). Simplified StringBlock code to use a pre-build list of strings extracted from the string pool rather than ignoring the prebuilt list and re-extacting strings individually from the string pool for each getRaw() call.

---
+++ third-party/axmlparser/src/test/java/com/squareup/spoon/internal/thirdparty/axmlparser/StringBlockTests.java
@@ -0,0 +1,180 @@
+package com.squareup.spoon.internal.thirdparty.axmlparser;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class StringBlockTests {
+    private static final String APK_MANIFEST_UTF_8 = "file/manifest-utf-8.apk";
+    private static final String APK_MANIFEST_UTF_16 = "file/manifest-utf-16.apk";
+
+
+    private static final String PATH_TO_RESOURCES = "third-party/axmlparser/src/test/res/";
+
+    private static final String[] EXPECTED_STRINGS_UTF_8 = new String[]{
+            "label",
+            "name",
+            "targetPackage",
+            "handleProfiling",
+            "functionalTest",
+            "minSdkVersion",
+            "targetSdkVersion",
+            "application",
+            "android.test.runner",
+            "uses-library",
+            "Intents",
+            "android",
+            "edu.vanderbilt.demoapp",
+            "android.support.test.runner.AndroidJUnitRunner",
+            "Tests for edu.vanderbilt.demoapp",
+            "instrumentation",
+            "uses-sdk",
+            "edu.vanderbilt.demoapp.test",
+            "package",
+            "manifest",
+            "http://schemas.android.com/apk/res/android"
+    };
+
+    private static final String[] EXPECTED_STRINGS_UTF_16 = new String[]{
+            "minSdkVersion",
+            "targetSdkVersion",
+            "name",
+            "functionalTest",
+            "handleProfiling",
+            "label",
+            "targetPackage",
+            "debuggable",
+            "android",
+            "http://schemas.android.com/apk/res/android",
+            "",
+            "package",
+            "platformBuildVersionCode",
+            "platformBuildVersionName",
+            "manifest",
+            "edu.vanderbilt.demoapp.test",
+            "25",
+            "7.1.1",
+            "uses-sdk",
+            "instrumentation",
+            "android.support.test.runner.AndroidJUnitRunner",
+            "Tests for edu.vanderbilt.demoapp",
+            "edu.vanderbilt.demoapp",
+            "application",
+            "Intents",
+            "uses-library",
+            "android.test.runner"
+    };
+
+    @Test
+    public void testParseApk_Utf_8() throws Exception {
+        testParseApk(APK_MANIFEST_UTF_8);
+    }
+
+    @Test
+    public void testParseApk_Utf_16() throws Exception {
+        testParseApk(APK_MANIFEST_UTF_16);
+    }
+
+    @Test
+    public void testApkForExpectedStringsInStringPool_Utf8() throws Exception {
+        testApkForExpectedStringsInStringPool(APK_MANIFEST_UTF_8, EXPECTED_STRINGS_UTF_8);
+    }
+
+    @Test
+    public void testApkForExpectedStringsInStringPool_Utf16() throws Exception {
+        testApkForExpectedStringsInStringPool(APK_MANIFEST_UTF_16, EXPECTED_STRINGS_UTF_16);
+    }
+
+    private File getTestDataFile(String apkPath) throws FileNotFoundException {
+        File resourcesDirectory = new File(PATH_TO_RESOURCES);
+        assert(resourcesDirectory.isDirectory());
+        return new File(resourcesDirectory, apkPath);
+    }
+
+    public void testParseApk(String apkFilePath) throws Exception {
+        File apkTestFile = getTestDataFile(apkFilePath);
+
+        try (ZipFile zip = new ZipFile(apkTestFile)) {
+            ZipEntry entry = zip.getEntry("AndroidManifest.xml");
+
+            InputStream inputStream = zip.getInputStream(entry);
+            AXMLParser parser = new AXMLParser(inputStream);
+            int eventType = parser.getType();
+
+            boolean isManifest;
+            boolean isUsesSdk;
+            boolean isInstrumentation;
+
+            String appPackage = null;
+            Integer minSdkVersion = null;
+            String testPackage = null;
+            String testRunnerClass = null;
+
+            while (eventType != AXMLParser.END_DOCUMENT) {
+                if (eventType == AXMLParser.START_TAG) {
+                    String parserName = parser.getName();
+                    isManifest = "manifest".equals(parserName);
+                    isUsesSdk = "uses-sdk".equals(parserName);
+                    isInstrumentation = "instrumentation".equals(parserName);
+                    if (isManifest || isInstrumentation || isUsesSdk) {
+                        for (int i = 0; i < parser.getAttributeCount(); i++) {
+                            String parserAttributeName = parser.getAttributeName(i);
+                            if (isManifest && "package".equals(parserAttributeName)) {
+                                testPackage = parser.getAttributeValueString(i);
+                            } else if (isUsesSdk && "minSdkVersion".equals(parserAttributeName)) {
+                                minSdkVersion = parser.getAttributeValue(i);
+                            } else if (isInstrumentation && "targetPackage".equals(parserAttributeName)) {
+                                appPackage = parser.getAttributeValueString(i);
+                            } else if (isInstrumentation && "name".equals(parserAttributeName)) {
+                                testRunnerClass = parser.getAttributeValueString(i);
+                            }
+                        }
+                    }
+                }
+                eventType = parser.next();
+            }
+
+            assertNotNull("Could not find minSdkVersion.", minSdkVersion);
+            assertNotNull("Could not find test application package.", testPackage);
+            assertNotNull("Could not find application package.", appPackage);
+            assertNotNull("Could not find test runner class.", testRunnerClass);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Common test helper that is used to test both UTF-8 and UTF-16 input data and expected results.
+     *
+     * @param inputTestFilePath Input file path.
+     * @param expectedStrings   String array containing expected string results.
+     */
+    private void testApkForExpectedStringsInStringPool(String inputTestFilePath, String[] expectedStrings)
+            throws IOException {
+        File apkTestFile = getTestDataFile(inputTestFilePath);
+        assertTrue(apkTestFile.exists());
+        try (ZipFile zip = new ZipFile(apkTestFile)) {
+            ZipEntry entry = zip.getEntry("AndroidManifest.xml");
+            InputStream inputStream = zip.getInputStream(entry);
+
+            // Skip over chunk header so that we can directly call StringBlock class.
+            ReadUtil.readInt(inputStream);
+            ReadUtil.readInt(inputStream);
+
+            StringBlock stringBlock = StringBlock.read(new IntReader(inputStream, false));
+
+            assertEquals(expectedStrings.length, stringBlock.getCount());
+
+            for (int i = 0; i < stringBlock.getCount(); i++) {
+                assertEquals("String in string pool should match expected string.", expectedStrings[i], stringBlock.getRaw(i));
+            }
+        }
+    }
+}
