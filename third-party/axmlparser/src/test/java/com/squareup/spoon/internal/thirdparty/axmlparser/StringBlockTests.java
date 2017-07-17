e8770b31e5b4e0110d6cab8556272aec690cef68
Monte <montecreasor@gmail.com>
2017-07-17 15:01:29 -0400

    Forget to commit axmlparser build.gradle that contains a new dependency 'testCompile deps.junit' required for the new StringBlockText JUnit test.
    Added method JavaDoc for all test methods.

--- third-party/axmlparser/src/test/java/com/squareup/spoon/internal/thirdparty/axmlparser/StringBlockTests.java
+++ third-party/axmlparser/src/test/java/com/squareup/spoon/internal/thirdparty/axmlparser/StringBlockTests.java
@@ -11,12 +11,14 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class StringBlockTests {
+    /** Test APKs are located in axmlparser's res directory */
     private static final String APK_MANIFEST_UTF_8 = "file/manifest-utf-8.apk";
     private static final String APK_MANIFEST_UTF_16 = "file/manifest-utf-16.apk";
 
-
+    /** Path the resource directory for axmlparser (relative to project root). */
     private static final String PATH_TO_RESOURCES = "third-party/axmlparser/src/test/res/";
 
+    /** Strings that should be string pool in the test file APK_MANIFEST_UTF_8. */
     private static final String[] EXPECTED_STRINGS_UTF_8 = new String[]{
             "label",
             "name",
@@ -41,6 +43,7 @@ public class StringBlockTests {
             "http://schemas.android.com/apk/res/android"
     };
 
+    /** Strings that should be string pool in the test file APK_MANIFEST_UTF_16 . */
     private static final String[] EXPECTED_STRINGS_UTF_16 = new String[]{
             "minSdkVersion",
             "targetSdkVersion",
@@ -71,33 +74,72 @@ public class StringBlockTests {
             "android.test.runner"
     };
 
+    /**
+     * Tests UTF-8 APK parsing using same code as spoon-runner's
+     * SpoonInstrumentationInfo@parserFromFile.
+     *
+     * @throws Exception Any test exception.
+     */
     @Test
     public void testParseApk_Utf_8() throws Exception {
         testParseApk(APK_MANIFEST_UTF_8);
     }
 
+    /**
+     * Tests UTF-16 APK parsing using same code as spoon-runner's
+     * SpoonInstrumentationInfo@parserFromFile.
+     *
+     * @throws Exception Any test exception.
+     */
     @Test
     public void testParseApk_Utf_16() throws Exception {
         testParseApk(APK_MANIFEST_UTF_16);
     }
 
+    /**
+     * Tests UTF-8 APK string pool contents against expected
+     * static UTF-8 string list.
+     *
+     * @throws Exception Any test exception.
+     */
     @Test
     public void testApkForExpectedStringsInStringPool_Utf8() throws Exception {
         testApkForExpectedStringsInStringPool(APK_MANIFEST_UTF_8, EXPECTED_STRINGS_UTF_8);
     }
 
+    /**
+     * Tests UTF-16 APK string pool contents against expected
+     * static UTF-16 string list.
+     *
+     * @throws Exception Any test exception.
+     */
     @Test
     public void testApkForExpectedStringsInStringPool_Utf16() throws Exception {
         testApkForExpectedStringsInStringPool(APK_MANIFEST_UTF_16, EXPECTED_STRINGS_UTF_16);
     }
 
+    /**
+     * Helper that constructs File object for test APK files located
+     * in test/res/file directory.
+     *
+     * @param apkPath Path to either a UTF-8 or UTF-9 style APK test resource.
+     * @return File object for APK test resource.
+     * @throws FileNotFoundException Thrown when test file is not found.
+     */
     private File getTestDataFile(String apkPath) throws FileNotFoundException {
         File resourcesDirectory = new File(PATH_TO_RESOURCES);
         assert(resourcesDirectory.isDirectory());
         return new File(resourcesDirectory, apkPath);
     }
 
-    public void testParseApk(String apkFilePath) throws Exception {
+    /**
+     * Helper method that is a copy of the parseFromFile method in
+     * spoon-runner's InstrumentationInfo class.
+     *
+     * @param apkFilePath File to test (from test resources)
+     * @throws Exception Any test exception.
+     */
+    private void testParseApk(String apkFilePath) throws Exception {
         File apkTestFile = getTestDataFile(apkFilePath);
 
         try (ZipFile zip = new ZipFile(apkTestFile)) {
@@ -151,10 +193,12 @@ public class StringBlockTests {
     }
 
     /**
-     * Common test helper that is used to test both UTF-8 and UTF-16 input data and expected results.
+     * Helper method that is used to test both UTF-8 and UTF-16 input data against
+     * expected results stored in static string arrays..
      *
      * @param inputTestFilePath Input file path.
      * @param expectedStrings   String array containing expected string results.
+     * @throws IOException Any test exception.
      */
     private void testApkForExpectedStringsInStringPool(String inputTestFilePath, String[] expectedStrings)
             throws IOException {
