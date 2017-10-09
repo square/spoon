1a42e920346d6bac037b1162d6fc11ea609f1ee7
Peter Hewitt <peter.hewitt@cardinalhealth.com>
2017-10-09 14:21:51 -0400

    Remove extra ws line

--- spoon-client/src/main/java/com/squareup/spoon/SpoonRule.java
+++ spoon-client/src/main/java/com/squareup/spoon/SpoonRule.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.Log;
-
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
